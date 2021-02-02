c5d9f047295b2f0d25006ea6070e42c2fe749e02
StuartCox3107 <coxstuart@hotmail.com>
2021-02-02 16:17:25 +0000

    all html pages added and basic contact form

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -31,3 +31,23 @@ jQuery(function($) {
 	
 	adjustNav();
 });
+
+// Back to top button
+mybutton = document.getElementById("myBtn");
+
+// Show on scroll
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Scroll to top on click
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
