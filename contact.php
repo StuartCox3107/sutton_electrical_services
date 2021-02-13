c746c1ac0e2dfec317c0ebfbeca59c28dd4af43c
StuartCox3107 <coxstuart@hotmail.com>
2021-02-13 19:39:43 +0000

    services basic outline added

--- contact.php
+++ contact.php
@@ -1,21 +1,20 @@
+Thanks for your message, we'll be in touch soon!
+
 <?php 
-if(isset($_POST['submit'])){
-    $to = "coxstuart@hotmail.com.com"; // this is your Email address
-    $from = $_POST['Email']; // this is the sender's Email address
-    $Name = $_POST['Name'];
-    $Email = "Email";
-    $Phone = "Phone";
-    $Message = "Message";
-    $Message = $Name . " " . $Email . " wrote the following:" . "\n\n" . $_POST['Message'];
-    $message2 = "Here is a copy of your message " . $Name . "\n\n" . $_POST['Message'];
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$Message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you for getting in touch" . $Name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+    $userName 		= $_POST['Name'];
+    $userEmail	 	= $_POST['Email'];
+    $userPhone	 	= $_POST['Phone'];
+	$userMessage 		= $_POST['Message'];
+
+	$to 			= "coxstuart@hotmail.com";
+	$subject 		= "Web enquiry";
+	$body 			= "Information Submitted:";
+
+	$body .= "\r\n Name: " . $userName;
+	$body .= "\r\n Email: " . $userEmail;
+	$body .= "\r\n Message: " . $userMessage;
+
+	mail($to, $subject, $body);
+
 ?>
-
-
